66e486178a45ad4806811282fb91ab02e6035c79
SamuelLabrador <sam.labrador@Yahoo.com>
2018-11-07 22:10:57 -0800

    working version

--- A3/3-3_v2.py
+++ A3/3-3_v2.py
@@ -1,15 +1,14 @@
-import numpy as np 
+import numpy as np
 
 A = np.array([4, 5, 17, 23, 11, 2])
 sz = len(A)
 
 Table = np.zeros((sz, sz))
 
-T = 29
+T = 44
 
 for i in range(sz): #start from left most, move to right column
-	for j in range(i, -1, -1):
-		#if adding same number to itself, set to column
+	for j in range(i, -1, -1): #move through rows
 		if j == i:
 			if A[i] > T:
 				Table[j, i] = 0
@@ -18,15 +17,11 @@ for i in range(sz): #start from left most, move to right column
 		else:
 			val = 0
 			for k in range(j + 1, i + 1, 1):
-				# print('j:{} k:{} i:{}'.format(j, k, i))
-				#find previous highest value
-				if Table[k, i] > val:
-					 val = Table[k, i]
+				if Table[k, i] + A[j] <= T and Table[k, i] + A[j] > val:
+					val = Table[k, i] + A[j]
 
-			if A[j] + val <= T:
-				Table[j, i] = A[j] + val
-			else:
-				Table[j, i] = val
+			Table[j, i] = val
+
 print('looking for {}'.format(T))
 print(A)
-print(Table)
+print(Table) 
